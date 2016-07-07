87c6b7392b083a4ea63df94e9b95f7aa3396501c
zhouxianjun <gary@cn-face.com>
2016-07-08 00:04:00 +0800

    init

---
+++ data-mgr-common/src/main/java/com/alone/entity/Role.java
@@ -0,0 +1,31 @@
+package com.alone.entity;
+
+import lombok.Data;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author zhouxianjun(Alone)
+ * @ClassName:
+ * @Description:
+ * @date 16-7-7 下午9:00
+ */
+@Data
+@Table(name = "t_role")
+public class Role implements Serializable {
+    @Id
+    private Long id;
+
+    private Long pid;
+
+    private String name;
+
+    private Boolean status;
+
+    private Date create_time;
+
+    private Date update_time;
+}
---
+++ data-mgr-core/src/main/java/com/alone/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.alone.mapper;
+
+import com.alone.entity.User;
+import com.github.abel533.mapper.Mapper;
+
+/**
+ * @author zhouxianjun(Alone)
+ * @ClassName:
+ * @Description:
+ * @date 16-7-7 下午9:55
+ */
+public interface UserMapper extends Mapper<User> {
+}
